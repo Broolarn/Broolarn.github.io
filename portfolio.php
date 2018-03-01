32535f8aab711966ba559ac929f39848aedad558
Julius Kördel <kordel.julius@gmail.com>
2018-03-01 08:39:02 +0100

    Portfolio

---
+++ portfolio.php
@@ -0,0 +1,38 @@
+{
+"records": [
+  {
+    "title":"BatApp",
+    "description": "<p> Managed to create a none destructing voice changer that uses smart sampling method to change pitch of voice to sound as batman. The app is still very fast but not a realtime application if not backed up by good hardware. </p>",
+    "skills": "<span class='programming'>Voice changer</span>, <span class='technique'>Grafics</span>, <span class='programming'>Matlab</span>",
+    "category": "Voice changer",
+    "duration": "Autumn 2017", 
+    "links": "<a href='Grupp 13 .pdf' target='_blank'>Project report (English)</a><br><a href='https://github.com/Broolarn' target='_blank'>Source code</a><br>", 
+    "video": "<img class='img-rounded' src='../bilder/batappbild.jpg' alt='Screenshot from batapp.' />",
+    "show": "true"
+  },
+
+  {
+    "title":"Walking Sim",
+    "description": "<p>    The base plan for this project was to create a environment that the player could walk around in and explore. The environment would tell a story of how he ended up where he is and why he is alone. For the time given for this schoolproject the result was very pleasing and we managed to deliver the feeling to the player that we strived for </p>",
+    "skills": "<span class='programming'>Blueprints</span>, <span class='technique'>Grafics</span>, <span class='programming'>Unreal Engine</span>, <span class='technique'>Modeling</span>, <span class='programming'>Textures</span>",
+    "category": "Unreal, Computer graphics",
+    "duration": "Spring 2017", 
+    "links": "<a href='testpdf.pdf' target='_blank'>Project report (English)</a><br><a href='https://github.com/Broolarn' target='_blank'>Source code</a><br>", 
+    "video": "<img class='img-rounded' src='../bilder/walkingsimbild.png' alt='Screenshot from Walking sim.' />",
+    "show": "true"
+  },
+  
+  {
+
+ 
+    "title":"Swiper", 
+    "description": "<p>This was the first big production I been a part of. We managed to produce a fun game where the user swipes given arrows in the right direction.</p>",
+    "skills": "<span class='programming'>JavaScript</span>, <span class='technique'>Game programming</span>,   <span class='programming'>Phaser</span>",
+    "category": "Game,Phaser",
+    "duration": "Autumn 2016", 
+    "links": "<a href='.../lankar/ProjektRapportgruppswiper.pdf' target='_blank'>Project report (Swedish)</a><br><a href='https://github.com/Broolarn' target='_blank'>Source code</a><br>", 
+    "video": "<img class='img-rounded' src='../bilder/GIFgameover.gif' alt='Gameplay'  height='300'/>",
+    "show": "true"
+  
+  }]
+}
