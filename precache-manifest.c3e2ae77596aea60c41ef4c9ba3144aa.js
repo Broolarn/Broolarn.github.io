9f855f061b3249434c8332bd1ac2b314c06cbcfe
Julius Kördel <kordel.julius@gmail.com>
2020-11-22 14:13:27 +0100

    Updates

---
+++ precache-manifest.c3e2ae77596aea60c41ef4c9ba3144aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "310c0227b2fc71a3ed7a1a8e62274a02",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "368e24718dd330834d41",
+    "url": "/portfolio/static/js/2.bcf9eea4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/portfolio/static/js/2.bcf9eea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ab90147a81f28f4efc",
+    "url": "/portfolio/static/js/main.53d72d01.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "158efc03f51ef5acede1501af5b48a4c",
+    "url": "/portfolio/static/media/Julius.158efc03.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//broolarn.github.io/portfolio/precache-manifest.38b0064e5b7e0a41ef671133c1c9ed37.js"
+  "/portfolio/precache-manifest.c3e2ae77596aea60c41ef4c9ba3144aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//broolarn.github.io/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
