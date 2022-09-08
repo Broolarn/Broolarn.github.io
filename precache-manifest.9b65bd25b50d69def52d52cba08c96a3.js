798b04ead48de6974926d75524c8dcd1d45dba8d
Julius Kördel <kordel.julius@gmail.com>
2022-09-08 22:48:25 +0200

    Updates

---
+++ precache-manifest.9b65bd25b50d69def52d52cba08c96a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef8c80c2bfc458065efd48ddb433641",
+    "url": "/Broolarn.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "368e24718dd330834d41",
+    "url": "/Broolarn.github.io/portfolio/static/js/2.bcf9eea4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Broolarn.github.io/portfolio/static/js/2.bcf9eea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bcd16377330b59cfa15",
+    "url": "/Broolarn.github.io/portfolio/static/js/main.f619692f.chunk.js"
+  },
+  {
+    "revision": "2304e141a1cd28632a4e",
+    "url": "/Broolarn.github.io/portfolio/static/js/runtime-main.2adc6335.js"
+  },
+  {
+    "revision": "158efc03f51ef5acede1501af5b48a4c",
+    "url": "/Broolarn.github.io/portfolio/static/media/Julius.158efc03.jpg"
+  }
+]);
