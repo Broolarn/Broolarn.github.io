180c1d8be9dd06b9739e03cc1bb59eca6398e8ec
Julius Kördel <kordel.julius@gmail.com>
2022-09-08 23:15:53 +0200

    Updates

---
+++ precache-manifest.44f7ff45db7e05dff01cbb2098209aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673ae6d7eb2169f009b503c36c419903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "368e24718dd330834d41",
+    "url": "/static/js/2.bcf9eea4.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.bcf9eea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b346bcf895404bac6f42",
+    "url": "/static/js/main.8c010857.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "158efc03f51ef5acede1501af5b48a4c",
+    "url": "/static/media/Julius.158efc03.jpg"
+  }
+]);
